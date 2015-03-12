ff4855f94752997dc6d566ea75ccda3da0f1a8cf
Laura Trigeiro <laura@studentblueprint.org>
2015-03-11 22:49:54 -0700

    Use app.listen which is a shortcut for calling http.Server

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 var express = require('express');
-var http = require('http');
+//var http = require('http');
 var path = require('path');
 var socket_io = require('socket.io');
 
 var app = express();
-var http_server = http.Server(app);
-var io = socket_io(http_server);
+//var http_server = http.Server(app);
+var port = process.env.PORT || 3000;
+app.set('port', port);
+var server = app.listen(port);
+console.log("App listening on port " + port);
+
+var io = socket_io.listen(server);  //http_server);
 
 app.use('/public', express.static(__dirname + '/public'));
 app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap'))
@@ -23,9 +28,4 @@ io.on('connection', function(socket) {
 	socket.on('disconnect', function() {
 		console.log('user disconnected');
 	});
-});
-
-
-http_server.listen(3000, function() {
-	console.log('listening on *:3000');
 });
